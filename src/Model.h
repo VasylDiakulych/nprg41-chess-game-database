4a0371291217afbf58baea94d4f05a35bea81a9c
VasylDiakulych <vasyadiakulych@gmail.com>
2026-02-21 13:04:37 +0100

    created GameData constructor for Game

--- src/Model.h
+++ src/Model.h
@@ -4,43 +4,88 @@
 #include <string>
 #include <vector>
 
-namespace Pgn{
+namespace Pgn :: Model{
 
-    namespace Model {
+    enum class TimeCategory {
+        Bullet, 
+        Blitz,
+        Rapid,
+        Classical
+    };
 
-        class Game{
-        private:
-            //mandatory fiels
-            std::string event_name;
-            std::string site; // "City, Region COUNTRY" format, where COUNTRY is the three-letter International Olympic Committee code for the country
-            std::string date; // "YYYY.MM.DD", ????.??.?? is used for unknown values
-            std::string round;
-            std::string white_player_name; // "Lastname, Firstname"
-            std::string black_player_name;
-            std::string result;
+    struct GameData {
+        // mandatory fields
+        std::string event;
+        std::string site;
+        std::string date;
+        std::string round;
+        std::string white;
+        std::string black;
+        std::string result;
 
-            std::string moves;
+        // optional fields
+        std::optional<int> white_elo;
+        std::optional<int> black_elo;
+        std::optional<std::string> eco;
+        std::optional<std::string> opening;
+        std::optional<int> ply_count;
+        std::optional<std::string> time_control;
+    };
 
-            //optional fieldss
-            std::optional<int> white_elo;
-            std::optional<int> black_elo;
-            std::optional<std::string> ECO;
-            std::optional<std::string> opening;
-            std::optional<int> ply_count;
-            std::optional<std::string> time_control;
-            
+    struct GameConstants {
+        static constexpr size_t CHARS_PER_MOVE = 15; // 10 chars in the beginning, 20 closer to the end, 15 on average
+        static constexpr size_t TARGET_MOVE_COUNT = 80; // upper bound of an average length of the chess game
+        static constexpr size_t TERMINATION_BUFFER = 16; // "1-0" or "0-1" or "*" or "1/2-1/2", but we use more for safety
+        
+        static constexpr size_t INITIAL_MOVE_RESERVE = 
+            (CHARS_PER_MOVE * TARGET_MOVE_COUNT) + TERMINATION_BUFFER;
+    };
 
-        public:
-            Game();
-            ~Game() = default;
-        };  
+    class Game{
+    private:
+        //mandatory fiels
+        std::string event_name_;
+        std::string site_; // "City, Region COUNTRY" format, where COUNTRY is the three-letter International Olympic Committee code for the country
+        std::string date_; // "YYYY.MM.DD", ????.??.?? is used for unknown values
+        std::string round_;
+        std::string white_player_name_; // "Lastname, Firstname"
+        std::string black_player_name_;
+        std::string result_;
 
-        enum class TimeCategory {
-            Bullet, 
-            Blitz,
-            Rapid,
-            Classical
+        std::string moves_;
+
+        //optional fields
+        std::optional<int> white_elo_;
+        std::optional<int> black_elo_;
+        std::optional<std::string> ECO_;
+        std::optional<std::string> opening_;
+        std::optional<int> ply_count_;
+        std::optional<std::string> time_control_;
+        
+    public:
+
+        Game();
+        ~Game() = default;
+
+        Game(GameData&& data) :
+            event_name_(std::move(data.event)),
+            site_(std::move(data.site)),
+            date_(std::move(data.date)),
+            round_(std::move(data.round)),
+            white_player_name_(std::move(data.white)),
+            black_player_name_(std::move(data.black)),
+            result_(std::move(data.result)),
+
+            white_elo_(data.white_elo), // integer
+            black_elo_(data.black_elo), // integer
+            ECO_(std::move(data.eco)),
+            ply_count_(data.ply_count), // integer
+            time_control_(std::move(data.time_control))
+        {
+            moves_.reserve(GameConstants::INITIAL_MOVE_RESERVE); // reserving space for moves string, to improve performance
         };
-    }
+
+
+    };  
 
 }
